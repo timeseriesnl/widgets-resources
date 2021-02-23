4b2f5040779b116aa57ebcb5918e578ea3c0776b
Jordan Hunt <jordan.hunt@mendix.com>
2021-02-23 12:26:36 +0200

    feat(): validate piw versions - review comments

--- packages/tools/pluggable-widgets-tools/scripts/validate-version.js
+++ packages/tools/pluggable-widgets-tools/scripts/validate-version.js
@@ -1,6 +1,6 @@
-const parser = require("fast-xml-parser");
 const { readFile } = require("fs").promises;
 const { join } = require("path");
+const { parseStringPromise } = require("xml2js");
 
 main().catch(e => {
     console.error(e);
@@ -8,14 +8,17 @@ main().catch(e => {
 });
 
 async function main() {
-    const packageXmlAsJson = parser.parse((await readFile(join(process.cwd(), "src/package.xml"))).toString(), {
-        ignoreAttributes: false
-    });
-    const packageXmlVersion = packageXmlAsJson.package.clientModule["@_version"];
+    const packageXmlAsJson = await parseStringPromise(
+        (await readFile(join(process.cwd(), "src/package.xml"))).toString(),
+        {
+            ignoreAttrs: false
+        }
+    );
+    const packageXmlVersion = packageXmlAsJson.package.clientModule[0].$.version;
     const packageJson = JSON.parse((await readFile(join(process.cwd(), "package.json"))).toString());
     const packageJsonVersion = packageJson.version;
 
-    if (packageJsonVersion === packageXmlVersion) return;
+    if (packageJsonVersion && packageXmlVersion && packageJsonVersion === packageXmlVersion) return;
 
     throw new Error(`[${packageJson.name}] Package.json version does not match widget's package.xml version.`);
 }
